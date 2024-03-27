5ba7be58ae6989bd25bff7cd049d6623df8db8e8
Stijn Geysen <stijn.geysen@luminex.be>
2024-03-27 12:54:05 +0100

    Add dockerfile to create container for running the test tool

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -7,7 +7,7 @@ import os
 def check_interface_up(expected=True):
     interface_addrs = psutil.net_if_addrs().get(IFACE) or []
     up = socket.AF_INET in [snicaddr.family for snicaddr in interface_addrs]
-    assert up == expected, f'Interface {IFACE} is not in the expected link state'
+    assert up == expected, f'Interface {IFACE} is not in the expected link state (up = {expected})'
 
 
 def set_interface_link(up: bool):
