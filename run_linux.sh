eb6188d02a1c4374696e647153fcab0fb8cc7afe
Stijn Geysen <stijn.geysen@luminex.be>
2024-03-28 09:43:24 +0100

    Use macvlan docker network instead of host networking mode and skip link-up test if host is not Linux

--- run_linux.sh
+++ run_linux.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-TESTS=$1
+INTERFACE=$1
+TESTS=$2
 
 docker build -t igmptester docker
+
+docker network rm -f igmp-net
+docker network create -d macvlan -o parent=$INTERFACE igmp-net
 
 docker run \
     --rm -it \
     --name igmptester \
-    --network=host \
+    --network=igmp-net \
     --cap-add=NET_ADMIN \
     --mount type=bind,source="$(pwd)",target=/App \
     igmptester \
     $TESTS
+
+docker network rm -f igmp-net
