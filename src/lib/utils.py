338b39ff71057fec9080378a77e4a40bc5fbbc65
Stijn Geysen <stijn.geysen@luminex.be>
2024-04-22 14:50:17 +0200

    Skip link-up testing when running test in Docker container

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,9 +1,13 @@
 from configuration import IFACE
 import psutil
 import socket
+import os
 
 
 def check_interface_up(expected=True):
+    if os.environ.get('RUNNING_IN_DOCKER', False):
+        # When running inside a Docker container, the interface is always up.
+        return
     interface_addrs = psutil.net_if_addrs().get(IFACE) or []
     up = socket.AF_INET in [snicaddr.family for snicaddr in interface_addrs]
     assert up == expected, f'Interface {IFACE} is not in the expected link state (up = {expected})'
